f9fba29f4e6be05ca5668e6cd9c4e66a5f3a4530
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-01 19:55:23 -0800

    refactored form and welcome page

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,54 +1,60 @@
-import React, { useState } from 'react';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
-import './index.css';
+import React from 'react'
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
+import './index.css'
+import { useHistory, useRouteMatch } from 'react-router'
 
-const LoginSignup = (props) => {
+const LoginSignup = () => {
+  const { path } = useRouteMatch()
+  const history = useHistory()
 
-    const [path, setPath] = useState(props.match.url);
+  const validationSchema = Yup.object().shape({
+    user_name: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    password: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+  })
 
-    const back = () => {
-        props.history.push('/welcome')
-    }
-    const validationSchema = Yup.object().shape({
-        user_name: Yup.string()
-            .min(2, 'Too Short!')
-            .max(50, 'Too Long!')
-            .required('Required'),
-        password: Yup.string()
-            .min(2, 'Too Short!')
-            .max(50, 'Too Long!')
-            .required('Required'),
-    });
-    return (
-        <div className="container-margin">
-            {path === '/signin' ? <h1>Sign in below</h1> : <h1>New user? Register below.</h1>
-            }
-            <Formik
-                initialValues={{ user_name: "", password: "" }}
-                validationSchema={validationSchema}
-                onSubmit={(values, tools) => {
-                    console.log(values); // the object we need is in values
-                    tools.resetForm()
-                }}>
-                {({ errors, touched }) => (
-                    <Form>
-                        {errors.user_name && touched.user_name ? (
-                            <div>{errors.user_name}</div>
-                        ) : null}
-                        <Field type="text" name="user_name" placeholder="Username" />
-                        {errors.password && touched.password ? (
-                            <div>{errors.password}</div>
-                        ) : null}
-                        <Field type="password" name="password" placeholder="Password" />
-                        <button type="submit">Submit</button>
-                    </Form>
+  return (
+    <div className='container-margin'>
+      {path === '/signin' ? (
+        <h1>Sign in below</h1>
+      ) : (
+        <h1>New user? Register below.</h1>
+      )}
 
-                )}
-            </Formik>
-            <button type="submit" onClick={back}>Back</button>
-        </div>
-    )
+      <Formik
+        initialValues={{ user_name: '', password: '' }}
+        validationSchema={validationSchema}
+        onSubmit={(values, tools) => {
+          console.log(values) // the object we need is in values
+          tools.resetForm()
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            {errors.user_name && touched.user_name ? (
+              <div>{errors.user_name}</div>
+            ) : null}
+            <Field type='text' name='user_name' placeholder='Username' />
+            {errors.password && touched.password ? (
+              <div>{errors.password}</div>
+            ) : null}
+            <Field type='password' name='password' placeholder='Password' />
+            <button type='submit'>Submit</button>
+          </Form>
+        )}
+      </Formik>
+
+      <button type='button' onClick={() => history.goBack()}>
+        Back
+      </button>
+    </div>
+  )
 }
 
-export default LoginSignup;
+export default LoginSignup
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,32 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './index.css';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './index.css'
 
-const WelcomePage = (props) => {
-    return (
-        <div className="container-margin">
-            <h1>Welcome please log in or register if you havent allready</h1>
-            <Link to='/signin'>
-                <button type='submit'>Login</button>
-            </Link>
-            <Link to='/register'>
-                <button type='submit'>Register</button>
-            </Link>
+const WelcomePage = props => (
+  <div className='container-margin'>
+    <h1>Welcome please log in or register if you havent allready</h1>
+    <Link to='/signin'>
+      <button type='submit'>Login</button>
+    </Link>
+    <Link to='/register'>
+      <button type='submit'>Register</button>
+    </Link>
 
-            <p>Welcome these new users to the site:</p>
-            {props.peopleArr.map((person, i) => {
-                return (
-                    <>
-                        <ul key={i}>
-                            <li>{person.login.username}</li>
-                        </ul>
+    <p>Welcome these new users to the site:</p>
+    {props.peopleArr.map((person, i) => {
+      return (
+        <ul key={i}>
+          <li>{person.login.username}</li>
+        </ul>
+      )
+    })}
+  </div>
+)
 
-                    </>
-                )
-            })
-            }
-        </div>
-    )
-}
-
-export default WelcomePage;
+export default WelcomePage
