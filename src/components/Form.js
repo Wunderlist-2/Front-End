761ca04418fd93c3d2938cb716a6017afb750e9d
Jordan Devaney <jordandevaney28@gmail.com>
2020-03-02 22:50:44 -0500

    css styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,58 +5,58 @@ import './index.css'
 import { useHistory, useRouteMatch } from 'react-router'
 
 const LoginSignup = () => {
-  const { path } = useRouteMatch()
-  const history = useHistory()
+    const { path } = useRouteMatch()
+    const history = useHistory()
 
-  const validationSchema = Yup.object().shape({
-    user_name: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
-    password: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
-  })
+    const validationSchema = Yup.object().shape({
+        user_name: Yup.string()
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Username is required'),
+        password: Yup.string()
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Password is required'),
+    })
 
-  return (
-    <div className='container-margin'>
-      {path === '/signin' ? (
-        <h1>Sign in below</h1>
-      ) : (
-        <h1>New user? Register below.</h1>
-      )}
+    return (
+        <div className='container'>
+            {path === '/signin' ? (
+                <h1>Sign in below</h1>
+            ) : (
+                    <h1>New user? Register below.</h1>
+                )}
 
-      <Formik
-        initialValues={{ user_name: '', password: '' }}
-        validationSchema={validationSchema}
-        onSubmit={(values, tools) => {
-          // the object we need is in values
-          // need ternary to dispatch to correct api endpoint if using the same form
-          path === '/signin' ? console.log(values) : console.log(values)
-          tools.resetForm()
-        }}
-      >
-        {({ errors, touched }) => (
-          <Form>
-            {errors.user_name && touched.user_name && (
-              <div>{errors.user_name}</div>
-            )}
-            <Field type='text' name='user_name' placeholder='Username' />
-            {errors.password && touched.password && (
-              <div>{errors.password}</div>
-            )}
-            <Field type='password' name='password' placeholder='Password' />
-            <button type='submit'>Submit</button>
-          </Form>
-        )}
-      </Formik>
+            <Formik
+                initialValues={{ user_name: '', password: '' }}
+                validationSchema={validationSchema}
+                onSubmit={(values, tools) => {
+                    // the object we need is in values
+                    // need ternary to dispatch to correct api endpoint if using the same form
+                    path === '/signin' ? console.log(values) : console.log(values)
+                    tools.resetForm()
+                }}
+            >
+                {({ errors, touched }) => (
+                    <Form>
+                        {errors.user_name && touched.user_name && (
+                            <span>{errors.user_name}</span>
+                        )}
+                        <Field type='text' name='user_name' placeholder='Username' />
+                        {errors.password && touched.password && (
+                            <span>{errors.password}</span>
+                        )}
+                        <Field type='password' name='password' placeholder='Password' />
+                        <button className='btn' type='submit'>Submit</button>
+                    </Form>
+                )}
+            </Formik>
 
-      <button type='button' onClick={() => history.goBack()}>
-        Back
+            <button className='btn' type='button' onClick={() => history.goBack()}>
+                Back
       </button>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default LoginSignup
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -3,24 +3,24 @@ import { Link } from 'react-router-dom'
 import './index.css'
 
 const WelcomePage = props => (
-  <div className='container-margin'>
-    <h1>Welcome to Wunderlist 2</h1>
-    <Link to='/signin'>
-      <button type='button'>Login</button>
-    </Link>
-    <Link to='/register'>
-      <button type='button'>Register</button>
-    </Link>
+    <div className='container'>
+        <h1>Welcome to Wunderlist 2</h1>
+        <Link to='/signin'>
+            <button className='btn' type='button'>Login</button>
+        </Link>
+        <Link to='/register'>
+            <button className='btn' type='button'>Register</button>
+        </Link>
 
-    <p>Welcome these new users to the site:</p>
-    {props.peopleArr.map((person, i) => {
-      return (
-        <ul key={i}>
-          <li>{person.login.username}</li>
+        <p>Welcome these new users to the site:</p>
+        <ul>
+            {props.peopleArr.map((person, i) => {
+                return (
+                    <li key={i}>{person.login.username}</li>
+                )
+            })}
         </ul>
-      )
-    })}
-  </div>
+    </div>
 )
 
 export default WelcomePage
