1ca7ba0628a100f614512508ab94366d5e838fc5
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-03 17:51:09 -0800

    put not working yet

--- src/redux/thunks/index.js
+++ src/redux/thunks/index.js
@@ -30,11 +30,12 @@ export const register = values => async dispatch => {
 
 export const editTodo = todo => async dispatch => {
   try {
-    const { data } = await axiosWithBaseURL().put(`/api/todos/${todo.id}`, {
+    const { data } = await axiosWithBaseURL().put(`/todos/${todo.id}`, {
+      id: todo.id,
       title: todo.title,
       user_id: todo.user_id,
-      due_date: todo.due_date,
-      date_completed: todo.due_date,
+      due_date: todo.due_date || null,
+      date_completed: todo.due_date || null,
       completed: todo.completed,
     })
     dispatch(editTodoItem(data.todo))
