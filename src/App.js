081a2480e8ae0f53b9d3feee9bc32f137bb0f7e4
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-04 18:40:16 -0800

    fixed privateRoute

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import WelcomePage from './components/WelcomePage'
 import TodoList from './components/TodoList'
 import Form from './components/Form'
 import { PrivateRoute, SignInRoute } from './utils/authRoutes'
-import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -14,7 +13,7 @@ const App = () => {
         <SignInRoute exact path='/' component={WelcomePage} />
         <SignInRoute exact path='/signin' component={Form} />
         <SignInRoute exact path='/register' component={Form} />
-        <Route exact path='/todolist' render={props =><TodoList {...props}/>}/>
+        <PrivateRoute exact path='/home' component={TodoList} />
       </div>
     </Router>
   )
