fcdf72ca7904a779c48e6a0020f17dc62aa96c7d
michelangelo17 <michelangelo.markus@gmail.com>
2020-03-05 18:08:00 -0800

    all changes

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { editTodo } from '../redux/thunks'
 import NewTodoForm from './NewTodoForm'
@@ -6,30 +6,35 @@ import NewTodoForm from './NewTodoForm'
 const TodoList = () => {
   const { todos } = useSelector(state => state)
   const dispatch = useDispatch()
-   
-  const [searchTerm, setSearchTerm] = useState("");  
-  const [searchResults, setSearchResults] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState('')
+  const [searchResults, setSearchResults] = useState([])
 
   useEffect(() => {
     const results = todos.filter(todo =>
-      toto.title.toLowerCase().includes(searchTerm.toLowerCase())
-     );
-     setSearchResults(results);
-   }, [searchTerm]);
+      todo.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    setSearchResults(results)
+  }, [searchTerm, todos])
 
-   const handleChange = e => {
-    setSearchTerm(e.target.value);
-  };
+  const handleChange = e => {
+    setSearchTerm(e.target.value)
+  }
 
-      
   return (
     <div className='form-container'>
       <NewTodoForm />
-       
+
       <form>
-        <input className="searchForm"
-          id="name" type="text" name="textfield" placeholder="Search"
-          value={searchTerm} onChange={handleChange}/>
+        <input
+          className='searchForm'
+          id='name'
+          type='text'
+          name='textfield'
+          placeholder='Search'
+          value={searchTerm}
+          onChange={handleChange}
+        />
       </form>
 
       {searchResults.map(todo => {
